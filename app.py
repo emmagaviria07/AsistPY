9d95a5a7d52123517d2e960564e1d9321fe01ece
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-10-25 10:55:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ if user_question and ke and uploaded_file is not None:
             #format_response_for_streamlit(response)
             st.write(response)
     #except Exception as e:
-        st.error(f"Ocurrió un error al analizar los datos: {str(e)}")
+        #st.error(f"Ocurrió un error al analizar los datos: {str(e)}")
