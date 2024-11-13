1c60d35c8ade03f66f29447eb077a3d88d795a5b
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-11-13 16:53:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ if user_question and ke and uploaded_file is not None:
     try:
         with st.spinner('Analizando los datos...'):
             # Crear el agente con Claude y parámetros correctos
-            agent = create_pandas_dataframe_agent( ChatAnthropic(model='claude-3-haiku-20240307'),
+            agent = create_pandas_dataframe_agent( ChatAnthropic(model='claude-3-5-sonnet-20241022'), #claude-3-haiku-20240307
                 #Anthropic(
                 #    model="claude-2.1",
                 #    temperature=0,
